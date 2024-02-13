6da03fc83326baac10fd67e0876d36edfd9a42ce
aditya-sps <152249472+aditya-sps@users.noreply.github.com>
2024-02-13 11:26:03 +0530

    Bottom Sheet

--- App.tsx
+++ App.tsx
@@ -5,17 +5,27 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import SwipeFlatlist from './src/SwipeFlatlist/SwipeFlatlist';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import BottomTab from './src/BottomTab/BottomTab';
 
 function App(): React.JSX.Element {
+  const [visible, setVisible] = useState(false);
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{flex: 1}}>
-        <View style={{flex: 1}}>{/* <SwipeFlatlist /> */}</View>
+        <View style={{flex: 1}}>
+          {/* <SwipeFlatlist /> */}
+          {/* <BottomTab
+            visible={visible}
+            onClose={() => setVisible(false)}
+            height={400}>
+            <Text>Hi</Text>
+          </BottomTab> */}
+        </View>
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
--- src/BottomTab/BottomTab.tsx
+++ src/BottomTab/BottomTab.tsx
@@ -1,16 +1,100 @@
-import {Modal, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+import React, {useEffect} from 'react';
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
 
-const BottomTab = () => {
+interface BottomTabProps {
+  height: number;
+  visible: boolean;
+  onClose: () => void;
+  children: React.ReactElement;
+  containerStyle?: ViewStyle;
+}
+
+const BottomTab = (props: BottomTabProps) => {
+  const transitionY = useSharedValue(props.height);
+
+  useEffect(() => {
+    if (props?.visible) {
+      transitionY.value = withTiming(0);
+    } else {
+      transitionY.value = props.height;
+    }
+  }, [props.visible]);
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startY = transitionY.value;
+    },
+    onActive: (event, ctx) => {
+      let slidingValue = ctx.startY + event.translationY;
+      transitionY.value = Math.max(slidingValue, 0);
+    },
+    onEnd: (event, ctx) => {
+      let slidingValue = ctx.startY + event.translationY;
+      if (slidingValue < props.height / 2) {
+        transitionY.value = withTiming(0);
+      } else {
+        transitionY.value = withTiming(props.height);
+        runOnJS(props?.onClose)?.();
+      }
+    },
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: transitionY.value}],
+    };
+  }, []);
+
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={true}
-      onRequestClose={() => {}}></Modal>
+    <Modal animationType="fade" transparent={true} visible={props?.visible}>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <Pressable style={styles.wrapper}>
+          <PanGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View
+              style={[
+                styles.container,
+                props?.containerStyle,
+                {height: props?.height},
+                containerStyle,
+              ]}>
+              {props?.children}
+            </Animated.View>
+          </PanGestureHandler>
+        </Pressable>
+      </GestureHandlerRootView>
+    </Modal>
   );
 };
 
 export default BottomTab;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'flex-end',
+  },
+  container: {
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+});
